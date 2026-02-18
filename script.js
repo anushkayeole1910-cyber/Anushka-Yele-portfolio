2d641a5a87f5d574e490672b8975d69e3cfd854b
anushkayeole1910-cyber <anushkayeole1910@gmail.com>
2026-02-18 18:36:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const glow = document.querySelector(".cursor-glow");
 document.addEventListener("mousemove", e=>{
 glow.style.left = e.pageX+"px";
 glow.style.top = e.pageY+"px";
+
 });
+<script>
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
+</script>
